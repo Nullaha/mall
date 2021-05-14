b74438c7f21cc84c5a12d4b6fd004de04a192c4f
xiawj <swdyihehe@qq.com>
2021-05-14 21:23:00 +0800

    '初始化目錄'

---
+++ src/components/common/tabbar/TabBar.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="tab-bar" >
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+import TabBarItem from "./TabBarItem.vue"
+export default {
+  name: 'TabBar',
+  components: {
+      TabBarItem,
+  }
+}
+</script>
+<style>
+#tab-bar{
+    display: flex;
+    background-color: antiquewhite;
+
+    position:fixed;
+    left: 0;
+    right: 0;
+    bottom: 0;
+
+    box-shadow: 0 -2px 10px rgba(100, 100, 100, 0.8);
+
+}
+
+</style>
